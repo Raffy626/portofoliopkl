e4cf82b9a56199b2c863e60703d491c99d5aa2d7
Raffy626 <151097377+Raffy626@users.noreply.github.com>
2025-02-10 14:25:58 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// document.getElementById('menu-btn').addEventListener('click', function() {
+//     document.getElementById('menu').classList.toggle('hidden');
+// });
+
+// document.querySelectorAll('.nav-link').forEach(anchor => {
+//     anchor.addEventListener('click', function(e) {
+//         e.preventDefault();
+//         const targetId = this.getAttribute('href').substring(1);
+//         document.getElementById(targetId).scrollIntoView({
+//             behavior: 'smooth'
+//         });
+//     });
+// });
